4564dc411e64f4c19d4dd58c8cb538883427d1da
judgewooden <douwe.jong@gmail.com>
2023-02-24 20:30:09 +0100

    Minor fix

--- boditrax.py
+++ boditrax.py
@@ -10,27 +10,27 @@ import browser_cookie3
 class Boditrax:
     """
         This class will hold the variables from the server
-        self._fields is a list of the variables
+        self.__fields is a list of the variables
     """
 
     def __init__( self, cookiejar: CookieJar = None):
-        self._fields: list = []
+        self.__fields: list = []
 
-        self._session = requests.Session()
-        self._session.headers.update({
+        self.__session = requests.Session()
+        self.__session.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
         })
         if cookiejar is not None:
-            self._session.cookies.update(cookiejar)
+            self.__session.cookies.update(cookiejar)
         else:
             for domain_name in ["boditrax.com", "cp.boditrax.com"]:
-                self._session.cookies.update(browser_cookie3.load(domain_name=domain_name))
+                self.__session.cookies.update(browser_cookie3.load(domain_name=domain_name))
 
     def load_from_string(self, json_string: str):
         """ Load data from json string """
         for key, value in json.loads(json_string).items():
             setattr(self, key, value)
-            self._fields.append(key)
+            self.__fields.append(key)
 
     def load_from_file(self, filename: str):
         """ Load data from json file """
@@ -40,7 +40,7 @@ class Boditrax:
     def save_to_file(self, filename: str):
         """ Save data to a json file """
         items = {}
-        for item in self._fields:
+        for item in self.__fields:
             items[item] = getattr(self, item)
         with open(filename, "w", encoding='utf-8') as file:
             json.dump(items, file, sort_keys=False, indent=2)
@@ -51,7 +51,7 @@ class Boditrax:
         result['date'] = pd.to_datetime(result['date'])
         result.set_index('date', inplace=True)
 
-        for item in self._fields:
+        for item in self.__fields:
             element = getattr(self, item)
             new = []
             for key, value in element['data']:
@@ -65,7 +65,7 @@ class Boditrax:
 
     def get_from_cloud(self):
         """ load the data from the chart element on boditrax website """
-        result = self._session.get('https://cp.boditrax.com/dashboard/track/lines')
+        result = self.__session.get('https://cp.boditrax.com/dashboard/track/lines')
         if result.ok:
             if result.headers["Content-Type"].startswith("application/json"):
                 self.load_from_string(result.content.decode("utf8"))
@@ -78,16 +78,17 @@ class Boditrax:
 if __name__ == "__main__":
     b = Boditrax()
     b.get_from_cloud()
-    # b.load_from_file(filename='lines5.json')
-    # b.save_to_file(filename='output.json')
     df = b.to_dataframe()
     df.to_csv("boditrax.csv")
 
-    # print("FIELDS:", b._fields)
-    # print("LENGTH", len(df))
+    # b.save_to_file(filename='output.json')
+    # b.load_from_file(filename='output.json')
+
+    print("LENGTH", len(df))
 
     # print(b.fatpercentage['name'])
     # print(b.fatpercentage['primaryUnit'])
     # print(b.fatpercentage)
 
-    print(df.tail())
+    print(df.tail(100))
+ 
